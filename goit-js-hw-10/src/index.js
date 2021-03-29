5605a2d232924073991ef03f6389bcc9110d2972
SmKaterina <katesemenenko55@gmail.com>
2021-03-29 22:18:36 +0300

    changes

--- goit-js-hw-10/src/index.js
+++ goit-js-hw-10/src/index.js
@@ -1,4 +1,29 @@
 import './styles.css';
+import menu from './menu.json';
+import template from './template.hbs';
+const body = document.querySelector('body');
+const listRef = document.querySelector('.js-menu');
+const checkBox = document.getElementById('theme-switch-toggle');
+const markup = template(menu);
+listRef.insertAdjacentHTML('afterbegin', markup);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-import './js/initTheme';
-import './js/initMarkup';
+if (localStorage.getItem('theme')) {
+  body.classList.add(Theme.DARK);
+  checkBox.checked = true;
+}
+
+checkBox.addEventListener('change', () => {
+  if(checkBox.checked) {
+  body.classList.add(Theme.DARK)
+  body.classList.remove(Theme.LIGHT)
+  localStorage.setItem('theme', "темная тема")
+  } else{
+    body.classList.add(Theme.LIGHT)
+    body.classList.remove(Theme.DARK)
+    localStorage.removeItem('theme')
+  }
+});
