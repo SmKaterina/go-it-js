7f8c2cc3419d073ada3987a57f7dffa2b106b6bc
SmKaterina <57391475+SmKaterina@users.noreply.github.com>
2021-03-29 17:58:01 +0300

    Update index.js

--- goit-js-hw-10/src/index.js
+++ goit-js-hw-10/src/index.js
@@ -1,27 +1,3 @@
 import './styles.css';
-import menu from './menu.json';
-import templates from './templates.hbs';
-const body = document.querySelector('body');
-const listRef = document.querySelector('.js-menu');
-const checkBox = document.getElementById('theme-switch-toggle');
-const markup = template(menu);
-listRef.insertAdjacentHTML('afterbegin', markup);
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-if (localStorage.getItem('theme')) {
-  body.classList.add(Theme.DARK);
-  checkBox.checked = true;
-}
-checkBox.addEventListener('change', () => {
-  if(checkBox.checked) {
-  body.classList.add(Theme.DARK)
-  body.classList.remove(Theme.LIGHT)
-  localStorage.setItem('theme', "темная тема")
-  } else{
-    body.classList.add(Theme.LIGHT)
-    body.classList.remove(Theme.DARK)
-    localStorage.removeItem('theme')
-  }
-});
+import './js/initTheme';
+import './js/initMurkup'
